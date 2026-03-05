7fd19ccb40c2c3ba4e45c5e62f4efbd83d285ab3
Christoph Spiegel <christophspiegel.berlin@gmail.com>
2026-03-05 12:52:36 +0100

    S04 exercise 1.3: revert to named hpqr

--- LeanBlockCourse26/P02_Logic/S04_Negation.lean
+++ LeanBlockCourse26/P02_Logic/S04_Negation.lean
@@ -195,9 +195,9 @@ example (P Q : Prop) (p : ¬¬P) (f : P → Q) : ¬¬Q := by
 
 -- Exercise 1.3
 example (P Q R : Prop) (h : P ∨ Q ∨ R → ¬(P ∧ Q ∧ R)) : (P ∨ Q) ∨ R → ¬((P ∧ Q) ∧ R) := by
-  intro _ ⟨⟨p, q⟩, r⟩
+  intro hpqr ⟨⟨p, q⟩, r⟩
   apply h
-  · rcases ‹(P ∨ Q) ∨ R› with hpq | r
+  · rcases hpqr with hpq | r
     · rcases hpq with p | q
       · left; exact p
       · right; left; exact q
